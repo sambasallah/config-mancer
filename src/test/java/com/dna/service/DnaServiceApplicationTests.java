5540ed0b2638853bf8cf3eb693d050393e1078fb
Samba Sallah <sambasallah10@gmail.com>
2024-12-20 18:33:31 -0600

    initial commit

---
+++ src/test/java/com/dna/service/DnaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dna.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
